b3b28c19612dd6e291f9ffae1cffc2c3ab24035e
Kyriakos Malamas <kmalamas@outlook.com>
2023-03-03 19:06:50 +0200

    remove 404

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,12 +8,6 @@ then
     wget -P $TEAMCITY_DATA_PATH/lib/jdbc https://jdbc.postgresql.org/download/postgresql-42.5.4.jar --no-check-certificate
 fi
 
-if [ ! -f "$TEAMCITY_DATA_PATH/plugins/webhooks.zip" ];
-then
-    echo "Downloading Webhook plugin..."
-    wget -P $TEAMCITY_DATA_PATH/plugins https://dl.bintray.com/cloudnative/teamcity/teamcity-webhooks/0.0.5/webhooks.zip
-fi
-
 if [ ! -f "$TEAMCITY_DATA_PATH/plugins/tcSlackNotificationsPlugin" ];
 then
     echo "Downloading Slack plugin..."
